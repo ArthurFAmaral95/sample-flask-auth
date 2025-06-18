1bec314c17416fd9bea08d987dfaaa714c84b05e
Arthur Amaral <arthuramaral6@gmail.com>
2025-06-18 07:11:11 -0300

    Modeling User class

---
+++ models/user.py
@@ -0,0 +1,8 @@
+from database import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+  #id (int), username (str), password (str)
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(80), nullable=False, unique=True) #valor dentro do String(80) é a quantidade maxima de caracteres e nullable é a permissao se a coluna pode estar vazia ou nao
+  password = db.Column(db.String(80), nullable=False)
