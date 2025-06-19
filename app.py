ad25086aab587332f5f44db8080374814141976a
Arthur Amaral <arthuramaral6@gmail.com>
2025-06-19 06:43:58 -0300

    Finishing login and auth route

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager #gerencia o login dos usuarios
+from flask_login import LoginManager, login_user, current_user #gerencia o login dos usuarios
+import os
+
+caminho_arquivo = 'modulo_4/sample-flask-auth/instance/database.db'
+caminho_absoluto = os.path.abspath(caminho_arquivo)
  
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' #caminho do banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:////{caminho_absoluto}' #caminho do banco de dados
 
 login_manager = LoginManager()
 
 db.init_app(app)
 login_manager.init_app(app)
+
+#login --> definie qual a rota o app deve usar como rota de login
+login_manager.login_view = 'login'
+
+#funcao necessaria para carregar o usuario e possibilitar que o flask verifique o usuario no banco todas as vezes que necessario
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(user_id)
 
 #rota para o usuario fazer o login
 @app.route('/login', methods=['POST'])
@@ -23,6 +35,8 @@ def login():
   if username and password:
     user = User.query.filter_by(username=username).first() #o metodo filter_by retorna uma lista, por isso temos que usar o first para retornar somente o primeiro valor encontrado
     if user and user.password == password:
+      login_user(user) #faz o login e a autenticacao do usuario
+      print(current_user.is_authenticated)
       return jsonify({'message':'Autenticação realizada com sucesso'})
 
   return jsonify({'message':'Credencias inválidas'}), 400
--- database.py
+++ database.py
@@ -5,10 +5,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 #iniciamos a instancia do SQLAlchemy
 db = SQLAlchemy()
+
+
 '''
+CRIANDO O BANCO DE DADOS
+
 para iniciar o banco de dados, vamos no terminal e damos o comando flask shell
 depois db.create_all() para iniciar uma session e criar tudo que fizemos --> toda vez que conectamos em um db para fazer alguma operacao, abrimos uma session e quando terminamos temos que fecha-la. cada db tem um limite de quantas sessions abertas ele suporta simultaneamente
 depois de criar usamos o db.session.commit() para mandar para o db o que foi feito na session
 ao terminar usamos o comando exit() para sair do flask shell e finalizar a session
 ao finalizar todo esse processo, uma pasta instance sera criada com o arquivo do banco de dados dentro
+'''
+
+''''
+ADICIONANDO REGISTROS NO BANCO DE DADOS
+abrir o terminal e iniciar o flask shell
+criar uma instancia da classe User --> user = User(username='username', password='password')
+adicionar a instancia --> db.session.add(user)
+fazer o commit para o banco --> db.session.commit()
+encerrar a sessao --> exit()
 '''
